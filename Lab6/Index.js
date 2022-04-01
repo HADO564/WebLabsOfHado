140b7c909c48d151ebaa3803cd08ac9e5caa6103
Haider Muhammad <70658014+HADO564@users.noreply.github.com>
2022-04-01 14:17:53 +0500

    Completed

--- Lab6/Index.js
+++ Lab6/Index.js
@@ -1,3 +1,28 @@
+let m=null;
+
+function MC()
+{
+    m=null;
+}
+
+function MS()
+{
+    m=document.getElementById('main').value;
+}
+
+function MR()
+{
+    document.getElementById('main').value=m;
+}
+
+function Mplus()
+{
+    m= m+'+'+ document.getElementById('main').value;
+    console.log("Mplus1")
+    solve2();
+    document.getElementById('main').value=m;
+}
+
 function decimal() {
     let x = document.getElementById('main').value;
     if (x == '')
@@ -21,12 +46,39 @@ function comp() {
 
 function solve(){
     let x = document.getElementById('main').value;
+    x=x.replace('^','**');
     x=eval(x);
     if(x=="Infinity")
     x="Math Error";
     document.getElementById('main').value=x;
 }
 
+function solve2(){
+    x=m;
+    x=x.replace('^','**');
+    x=eval(x);
+    if(x=="Infinity")
+    x="Math Error";
+    m=x;
+}
+
+
 function exp(){
     let x = document.getElementById('main').value;
+    document.getElementById('main').value+='^2';
+}
+
+function exp2(){
+    let x = document.getElementById('main').value;
+    document.getElementById('main').value+='^(1/2)';
+}
+function frac(){
+    let x = document.getElementById('main').value;
+    if(x=='')
+    {
+        x="1/";
+    }
+    else
+    x+='/'
+    document.getElementById('main').value=x;
 }
